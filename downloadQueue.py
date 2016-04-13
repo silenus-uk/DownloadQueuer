7128fcdda8e88bfd0a029569cf35e70d7c45ffca
silenus-uk <krisposkett@gmail.com>
2016-04-13 21:47:14 +0100

    Add Log and Time  stamp
    Add a log file with links and time stamps of starting download

--- downloadQueue.py
+++ downloadQueue.py
@@ -14,8 +14,12 @@ while(itemsLeft):
         data = fin.read().splitlines(True)
         print(data[0])
         webbrowser.open_new(data[0])
+    with open('downloadLog.txt','a') as fout:
+        fout.writelines(data[0])
+        fout.writelines("Download Started at %s\n" % time.ctime())
+        
     if(len(data)>1):
-        with open('DownloadQueue.txt', 'w') as fout:
+       with open('DownloadQueue.txt', 'w') as fout:
             fout.writelines(data[1:])
     else:
         with open('DownloadQueue.txt', 'w') as fout:
